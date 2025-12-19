dbd29cfe1f53bfd781fb86b61e5039f001d86138
VVoronkov <voronglass84@gmail.com>
2025-12-19 20:32:28 +0300

    Add alphabet.py

---
+++ ocr/alphabet.py
@@ -0,0 +1,8 @@
+ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+# CTC: 0 зарезервирован под blank
+char2idx = {char: idx + 1 for idx, char in enumerate(ALPHABET)}
+idx2char = {idx + 1: char for idx, char in enumerate(ALPHABET)}
+
+BLANK_IDX = 0
+NUM_CLASSES = len(ALPHABET) + 1
